4e21176ad211bafb5569aad80d9e0e296e1f0143
jakubszpilowski <jakub.szpilowski@student.pk.edu.pl>
2023-01-07 10:22:43 +0100

    Added Train, User and Ticket classes

---
+++ src/com/projekt/entity/Train.java
@@ -0,0 +1,17 @@
+package com.projekt.entity;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Train {
+    private int trainId;
+    private int trainNumber;
+    private String trainType;
+    private String from;
+    private String to;
+    private LocalTime departureTime;
+    private LocalTime arrivalTime;
+    private int platform;
+    private Date date;
+    private int numberOfSeats;
+}
